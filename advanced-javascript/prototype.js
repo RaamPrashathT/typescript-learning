5fe28f8e6048002a90d01f1a88979c9975505044
RaamPrashathT <131695872+RaamPrashathT@users.noreply.github.com>
2026-01-28 16:56:24 +0530

    prototypes

---
+++ advanced-javascript/prototype.js
@@ -0,0 +1,27 @@
+const animal = {
+    eats: "eating",
+    move() {
+        console.log("animal is moving")
+    }
+}
+
+const rabbit = {
+    hops: true,
+    __proto__: animal
+}
+
+console.log(rabbit.eats);
+rabbit.move();
+
+// constructor functions
+
+function greet(name, age) {
+    this.name
+    this.age
+}
+
+greet.prototype.move = function() {
+    console.log("greeting")
+}
+
+greet.prototype.move();
